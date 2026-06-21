5a9f870ef3827ea953d0162e601d3a1fcaebaeab
Addy Rodriguez <rdz.33.a@gmail.com>
2026-06-21 18:17:13 -0500

    Static Data Added

---
+++ src/data/flights.js
@@ -0,0 +1,27 @@
+const flights = [
+{
+    id: 1,
+    flightNumber: "SWA102",
+    route:"HOU -> DAL",
+    boardingStatus: "Boarding",
+    crewStatus: "Complete",
+    ready: true
+},
+{
+    id: 2,
+    flightNumber: "SWA105",
+    route: "HOU -> LGA",
+    boardingStatus: "Not Started",
+    crewStatus: "Complete", 
+    ready: true,
+},
+{
+    id: 3,
+    flightNumber: "SWA310",
+    route: "HOU -> PDX",
+    boardingStatus: "Delayed", 
+    crewStatus: "Short",
+    ready: false
+}
+]
+export default flights
