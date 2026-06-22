cee69bd5978aa4bb396ff4e89b43c08df79c9574
Addy Rodriguez <rdz.33.a@gmail.com>
2026-06-22 12:37:33 -0500

    Added crew, removed css

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1 className="text-2xl">GROUND CONTROL</h1>
+      <h1 className="text-3xl">GROUND CONTROL</h1>
       {flights.map((flight) => (
         <p key={flight.id}>
+          <h2 className="text-xl font-bold">Flight Status:</h2>
           <p>Flight Number: {flight.flightNumber}</p>
           <p>Route: {flight.route}</p>
           <p>Status: {flight.boardingStatus}</p>
-          <p>Crew Status: {flight.crewStatus}</p>
           <p>Flight Ready: {flight.ready ? "Yes" : "No"}</p>
+          <h2 className="text-xl font-bold">Crew</h2>
+          <p>Flight crew assigned: {flight.crew.assigned}</p>
+          <p>Flight crew required: {flight.crew.required}</p>
+          <p>Briefing complete: {flight.crew.briefingComplete ? "Yes" : "No"}</p>
           </p>
+
+        
       ))}
     </div>
   );
--- src/data/flights.js
+++ src/data/flights.js
@@ -4,24 +4,39 @@ const flights = [
     flightNumber: "SWA102",
     route:"HOU -> DAL",
     boardingStatus: "Boarding",
-    crewStatus: "Complete",
-    ready: true
+    ready: true,
+
+    crew: {
+        assigned: 4,
+        required: 4,
+        briefingComplete: true,
+    }
 },
 {
     id: 2,
     flightNumber: "SWA105",
     route: "HOU -> LGA",
-    boardingStatus: "Not Started",
-    crewStatus: "Complete", 
+    boardingStatus: "Not Started", 
     ready: true,
+
+    crew: {
+        assigned: 2,
+        required: 4,
+        briefingComplete: false,
+    }
 },
 {
     id: 3,
     flightNumber: "SWA310",
     route: "HOU -> PDX",
     boardingStatus: "Delayed", 
-    crewStatus: "Short",
-    ready: false
+    ready: false,
+
+    crew: {
+        assigned: 1,
+        required: 4,
+        briefingComplete: false,
+    }
 }
 ]
 export default flights
